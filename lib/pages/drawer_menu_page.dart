3dab0af192f1607d699098427454721c2177745d
jmapps <jmappsmuslim@gmail.com>
2021-08-17 16:19:07 +0300

    Fixes drawer widgets for portrait and landscape orientation

--- lib/pages/drawer_menu_page.dart
+++ lib/pages/drawer_menu_page.dart
@@ -1,4 +1,3 @@
-import 'package:clipboard/clipboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,169 +12,93 @@ class _DrawerMenuPageState extends State<DrawerMenuPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: Colors.blue,
-        image: DecorationImage(
-            colorFilter: new ColorFilter.mode(Colors.blue.withOpacity(0.3), BlendMode.dstATop),
-            image: AssetImage('assets/images/app_icon.png'),
-            fit: BoxFit.scaleDown),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(15),
+          bottomRight: Radius.circular(15),
+        ),
       ),
-      child: Center(
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+      child: SingleChildScrollView(
+        child: MediaQuery.of(context).orientation == Orientation.portrait
+            ? Column(
+                mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildItemButton('Краткое изложение', '/contents'),
-                  _buildItemButton('Имена', '/names'),
-                  _buildItemButton('Разъяснение имён', '/tafsirs'),
-                  _buildItemButton('Викторина', '/quiz'),
-                  _buildItemButton('Карточки', '/cards'),
-                  TextButton.icon(
-                    label: Text(
-                      'Поддержать проект',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                    icon: Icon(
-                      CupertinoIcons.bitcoin_circle,
+                  SizedBox(height: 32),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    iconSize: 230,
+                    onPressed: null,
+                    icon: Image.asset('assets/images/app_icon.png'),
+                  ),
+                  Text(
+                    'Толкование прекрасных\nимён Аллаха',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
                       color: Colors.white,
+                      fontWeight: FontWeight.w500,
                     ),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => new AlertDialog(
-                          title: Text(
-                            'Поддержать проект!',
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                          content: Flexible(
-                            flex: 1,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Card(
-                                  elevation: 1,
-                                  shadowColor: Colors.blue,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(8),
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        SelectableText(
-                                          'Bitcoin:\n\n13B9fMAxXFCSkLnkHFNrA2D2xAZtyMaWxy',
-                                          style: TextStyle(fontSize: 18),
-                                        ),
-                                        TextButton.icon(
-                                          icon: Icon(
-                                            CupertinoIcons.bitcoin_circle,
-                                            color: Colors.blue,
-                                          ),
-                                          label: Text(
-                                            'Скопировать адрес',
-                                            style: TextStyle(fontSize: 18),
-                                          ),
-                                          onPressed: () {
-                                            FlutterClipboard.copy(
-                                                '13B9fMAxXFCSkLnkHFNrA2D2xAZtyMaWxy');
-                                            ScaffoldMessenger.of(context)
-                                                .showSnackBar(
-                                              SnackBar(
-                                                backgroundColor: Colors.blue,
-                                                content: Text(
-                                                  'Адрес скопирован',
-                                                  style: TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                                duration:
-                                                    Duration(milliseconds: 500),
-                                              ),
-                                            );
-                                          },
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Card(
-                                  elevation: 1,
-                                  shadowColor: Colors.red,
-                                  child: Flexible(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: EdgeInsets.all(8),
-                                      child: Column(
-                                        children: [
-                                          SelectableText(
-                                            'Monero:\n\n87zyaJCihgg2r8mSbXKE1uj4iqPp8hqQkWMdr2SbBVmGchjVNACEDDtEV1tB79cK1q2bC8h8Sy3BMVQLfWCSfyBjLjqkJB7',
-                                            style: TextStyle(fontSize: 18),
-                                          ),
-                                          TextButton.icon(
-                                            icon: Icon(
-                                              CupertinoIcons
-                                                  .money_dollar_circle,
-                                              color: Colors.red,
-                                            ),
-                                            label: Text(
-                                              'Скопировать адрес',
-                                              style: TextStyle(
-                                                  fontSize: 18,
-                                                  color: Colors.red),
-                                            ),
-                                            onPressed: () {
-                                              FlutterClipboard.copy(
-                                                  '87zyaJCihgg2r8mSbXKE1uj4iqPp8hqQkWMdr2SbBVmGchjVNACEDDtEV1tB79cK1q2bC8h8Sy3BMVQLfWCSfyBjLjqkJB7');
-                                              ScaffoldMessenger.of(context)
-                                                  .showSnackBar(
-                                                SnackBar(
-                                                  backgroundColor: Colors.red,
-                                                  content: Text(
-                                                    'Адрес скопирован',
-                                                    style: TextStyle(
-                                                        color: Colors.white),
-                                                  ),
-                                                  duration: Duration(
-                                                      milliseconds: 500),
-                                                ),
-                                              );
-                                            },
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            MaterialButton(
-                              child: Text(
-                                'Закрыть',
-                                style:
-                                    TextStyle(color: Colors.red, fontSize: 18),
-                              ),
-                              onPressed: () {
-                                Navigator.of(context, rootNavigator: true)
-                                    .pop();
-                              },
-                            ),
-                          ],
+                  ),
+                  SizedBox(height: 32),
+                  _buildItemButton('Краткое изложение', '/contents'),
+                  SizedBox(height: 16),
+                  _buildItemButton('Имена', '/names'),
+                  SizedBox(height: 16),
+                  _buildItemButton('Разъяснение имён', '/tafsirs'),
+                  SizedBox(height: 16),
+                  _buildItemButton('Викторина', '/quiz'),
+                  SizedBox(height: 16),
+                  _buildItemButton('Карточки', '/cards'),
+                  SizedBox(height: 32),
+                ],
+              )
+            : Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 32),
+                      _buildItemButton('Краткое изложение', '/contents'),
+                      SizedBox(height: 16),
+                      _buildItemButton('Имена', '/names'),
+                      SizedBox(height: 16),
+                      _buildItemButton('Разъяснение имён', '/tafsirs'),
+                      SizedBox(height: 16),
+                      _buildItemButton('Викторина', '/quiz'),
+                      SizedBox(height: 16),
+                      _buildItemButton('Карточки', '/cards'),
+                      SizedBox(height: 32),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        iconSize: 250,
+                        onPressed: null,
+                        icon: Image.asset('assets/images/app_icon.png'),
+                      ),
+                      Text(
+                        'Толкование прекрасных\nимён Аллаха',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
                         ),
-                      );
-                    },
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ),
-          ),
-        ),
       ),
     );
   }
