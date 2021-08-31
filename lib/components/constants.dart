f28b9d62af47e9d73b5702647020dd5861e48b94
jmapps <jmappsmuslim@gmail.com>
2021-08-31 11:15:06 +0300

    Added constants class

---
+++ lib/components/constants.dart
@@ -0,0 +1,4 @@
+final String keyTrueArabicAnswer = 'key_true_arabic_answer';
+final String keyTrueRussianAnswer = 'key_true_russian_answer';
+final String keyLastArabicPage = 'last_arabic_page_view_page';
+final String keyLastRussianPage = 'last_russian_page_view_page';
