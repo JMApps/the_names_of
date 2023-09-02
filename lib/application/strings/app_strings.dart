b0467bce5daf627daa2c689085f423f817eca555
dev_muslim <jmappsmuslim@gmail.com>
2023-09-02 08:54:56 +0300

    Added colors, fixes theme for widgets

--- lib/application/strings/app_strings.dart
+++ lib/application/strings/app_strings.dart
@@ -1,9 +1,9 @@
 class AppStrings {
   static const String appName = 'Имена Аллаха';
 
-  static const String descriptionHead = 'Краткое изложение основ';
+  static const String descriptionHeads = 'Краткое изложение основ';
   static const String names = 'Имена';
-  static const String clarificationName = 'Разъяснение имён';
+  static const String clarificationNames = 'Разъяснение имён';
   static const String quiz = 'Викторина';
   static const String cards = 'Карточки';
   static const String jumpToContent = 'Перейти к содержимому';
