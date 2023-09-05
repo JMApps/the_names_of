42d2c2470eaa13b3a6ae32be64cd0133e9d7f228
dev_muslim <jmappsmuslim@gmail.com>
2023-09-05 10:43:52 +0300

    Change for main content

--- lib/application/state/main_contents_state.dart
+++ lib/application/state/main_contents_state.dart
@@ -1,19 +1,3 @@
 import 'package:flutter/material.dart';
 
-class MainContentsState extends ChangeNotifier {
-  final int contentId;
-  late final PageController _pageController;
-
-  MainContentsState([this.contentId = 0]) {
-    debugPrint('init = $contentId');
-    _pageController = PageController(initialPage: contentId);
-  }
-
-  PageController get getPageController => _pageController;
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-}
+class MainContentsState extends ChangeNotifier {}
--- lib/presentation/pages/main_content_page.dart
+++ lib/presentation/pages/main_content_page.dart
@@ -1,27 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:the_names_of/application/state/main_contents_state.dart';
 import 'package:the_names_of/application/strings/app_strings.dart';
-import 'package:the_names_of/presentation/lists/content_pages.dart';
+import 'package:the_names_of/application/styles/app_styles.dart';
+import 'package:the_names_of/data/local/database_query.dart';
+import 'package:the_names_of/domain/models/content_model.dart';
+import 'package:the_names_of/presentation/items/content_item.dart';
+import 'package:the_names_of/presentation/widgets/main_smooth_indicator.dart';
 
-class MainContentPage extends StatelessWidget {
+class MainContentPage extends StatefulWidget {
   const MainContentPage({super.key, required this.contentIndex});
 
   final int contentIndex;
 
   @override
+  State<MainContentPage> createState() => _MainContentPageState();
+}
+
+class _MainContentPageState extends State<MainContentPage> {
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: widget.contentIndex);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => MainContentsState(),
-        ),
-      ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(AppStrings.descriptionHeads),
-        ),
-        body: ContentPages(contentIndex: contentIndex),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.descriptionHeads),
+      ),
+      body: FutureBuilder<List<ContentModel>>(
+        future: DatabaseQuery().getAllContents(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<ContentModel>> snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(height: 8),
+                MainSmoothIndicator(
+                  controller: _pageController,
+                  count: snapshot.data!.length,
+                  dotColor: Colors.orange,
+                ),
+                const SizedBox(height: 8),
+                Expanded(
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final ContentModel model = snapshot.data![index];
+                      return ContentItem(model: model);
+                    },
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Padding(
+                padding: AppStyles.mainMarding,
+                child: Text(
+                  snapshot.error.toString(),
+                  style: const TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+        },
       ),
     );
   }
