36d3ec4cee3e4b770a3d3bc97c91e2fa46330812
dev_muslim <jmappsmuslim@gmail.com>
2023-09-06 12:43:17 +0300

    Added name screen widget

---
+++ lib/presentation/widgets/name_screen_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:the_names_of/application/styles/app_styles.dart';
+import 'package:the_names_of/domain/models/name_model.dart';
+
+class NameScreenWidget extends StatelessWidget {
+  const NameScreenWidget({super.key, required this.model});
+
+  final NameModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Padding(
+        padding: AppStyles.mainMardingMini,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              model.nameArabic,
+              style: const TextStyle(
+                fontSize: 22,
+                fontFamily: 'Khebrat',
+                color: Colors.red,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              model.nameTranscription,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Gilroy',
+                color: Colors.green,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              model.nameTranslation,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Gilroy',
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
