8e917413646884ac211527c85fc257f55004faac
dev_muslim <jmappsmuslim@gmail.com>
2024-04-30 18:43:11 +0300

    Fixes db service classes

--- lib/data/local/database_quiz_helper.dart
+++ lib/data/local/database_quiz_helper.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseQuizHelper {
@@ -22,34 +21,29 @@ class DatabaseQuizHelper {
   DatabaseQuizHelper.internal();
 
   Future<Database> initializeDatabase() async {
-    Directory? documentDirectory = Platform.isAndroid
-        ? await getExternalStorageDirectory()
-        : await getApplicationSupportDirectory();
+    const int dbVersion = 1;
+    const String sfqDatabaseName = 'app_quiz.db';
+    final databasePath = await getDatabasesPath();
+    String path = join(databasePath, sfqDatabaseName);
 
-    const String databaseName = 'app_quiz.db';
+    var database = await openDatabase(path);
 
-    String path = join(documentDirectory!.path, databaseName);
-    var exists = await databaseExists(path);
+    if (await database.getVersion() < dbVersion) {
+      database.close();
+      await deleteDatabase(path);
 
-    // String toDeleteDB = '${documentDirectory.path}/lessons_ramadan.db';
-    //
-    // var delDB = await databaseExists(toDeleteDB);
-    //
-    // if (delDB) {
-    //   await deleteDatabase(toDeleteDB);
-    // }
-
-    if (!exists) {
       try {
         await Directory(dirname(path)).create(recursive: true);
-      } catch (_) {
-        Exception('Invalid database');
-      }
+      } catch (_) {}
 
-      ByteData data = await rootBundle.load(join('assets/databases', databaseName));
+      ByteData data = await rootBundle.load(join('assets/databases', sfqDatabaseName));
       List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
       await File(path).writeAsBytes(bytes, flush: true);
+
+      database = await openDatabase(path);
+      database.setVersion(dbVersion);
     }
-    return await openDatabase(path);
+
+    return database;
   }
 }
