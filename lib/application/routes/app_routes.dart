09c5cd93860748d59dfd2ef35dccd73c5b3a1c4f
dev_muslim <jmappsmuslim@gmail.com>
2024-05-02 12:17:44 +0300

    Added route names class

--- lib/application/routes/app_routes.dart
+++ lib/application/routes/app_routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../data/models/arguments/main_args.dart';
+import '../../data/models/arguments/index_args.dart';
 import '../../data/models/arguments/quiz_mode_args.dart';
 import '../../presentation/pages/ar_ru_quiz_page.dart';
 import '../../presentation/pages/crib_page.dart';
@@ -11,50 +11,51 @@ import '../../presentation/pages/name_cards_page.dart';
 import '../../presentation/pages/quiz_score_page.dart';
 import '../../presentation/pages/ru_ar_quiz_page.dart';
 import '../../presentation/pages/settings_page.dart';
+import 'route_names.dart';
 
 class AppRoutes {
   static Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case 'main_contents_page':
-        final MainArgs args = routeSettings.arguments as MainArgs;
+      case RouteNames.mainContentsPage:
+        final IndexArgs indexArgs = routeSettings.arguments as IndexArgs;
         return MaterialPageRoute(
-          builder: (_) => MainContentPage(contentIndex: args.index),
+          builder: (_) => MainContentPage(contentIndex: indexArgs.index),
         );
-      case 'main_names_page':
-        final MainArgs args = routeSettings.arguments as MainArgs;
+      case RouteNames.mainNamesPage:
+        final IndexArgs indexArgs = routeSettings.arguments as IndexArgs;
         return MaterialPageRoute(
-          builder: (_) => MainNamesPage(nameIndex: args.index),
+          builder: (_) => MainNamesPage(nameIndex: indexArgs.index),
         );
-      case 'main_clarifications_page':
-        final MainArgs args = routeSettings.arguments as MainArgs;
+      case RouteNames.mainClarificationsPage:
+        final IndexArgs indexArgs = routeSettings.arguments as IndexArgs;
         return MaterialPageRoute(
-          builder: (_) => MainClarificationPage(clarificationIndex: args.index),
+          builder: (_) => MainClarificationPage(clarificationIndex: indexArgs.index),
         );
-      case 'app_settings_page':
+      case RouteNames.appSettingsPage:
         return MaterialPageRoute(
           builder: (_) => const SettingsPage(),
         );
-      case 'name_cards_page':
+      case RouteNames.nameCardsPage:
         return MaterialPageRoute(
           builder: (_) => const NameCardsPage(),
         );
-      case 'ar_ru_quiz_page':
+      case RouteNames.arRuQuizPage:
         return MaterialPageRoute(
           builder: (_) => const ArRuQuizPage(),
         );
-      case 'ru_ar_quiz_page':
+      case RouteNames.ruArQuizPage:
         return MaterialPageRoute(
           builder: (_) => const RuArQuizPage(),
         );
-      case 'quiz_score_page':
-        final QuizModeArgs args = routeSettings.arguments as QuizModeArgs;
+      case RouteNames.quizScorePage:
+        final QuizModeArgs quizModeArgs = routeSettings.arguments as QuizModeArgs;
         return MaterialPageRoute(
-          builder: (_) => QuizScorePage(quizMode: args.quizMode),
+          builder: (_) => QuizScorePage(quizMode: quizModeArgs.quizMode),
         );
-      case 'crib_page':
-        final QuizModeArgs args = routeSettings.arguments as QuizModeArgs;
+      case RouteNames.cribPage:
+        final QuizModeArgs quizModeArgs = routeSettings.arguments as QuizModeArgs;
         return MaterialPageRoute(
-          builder: (_) => CribPage(quizMode: args.quizMode),
+          builder: (_) => CribPage(quizMode: quizModeArgs.quizMode),
         );
       default:
         throw Exception('Invalid route ${routeSettings.name}');
